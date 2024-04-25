94aaef9ffcd6923167a18c562e3cb3c3b74a191e
Udath Hegde <udathhegde23@gmail.com>
2024-04-25 06:35:37 +0530

    Home page done!

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -9,6 +9,7 @@ import {
   NotificationsLogo,
   SearchLogo,
 } from "../../assets/constants";
+import { BiLogOut } from "react-icons/bi";
 
 const Sidebar = () => {
   const sidebarItems = [
@@ -38,7 +39,7 @@ const Sidebar = () => {
           alignItems="center"
           gap={2}
         >
-          <Avatar size={"sm"} name="Udath Hegde" src="/progilepic.png" />
+          <Avatar size={"sm"} name="Udath Hegde" src="/profilepic.png" />
         </Box>
       ),
       text: "Profile",
@@ -111,6 +112,34 @@ const Sidebar = () => {
             </Tooltip>
           ))}
         </Flex>
+		{/* LOGOUT */}
+		<Tooltip
+					hasArrow
+					label={"Logout"}
+					placement='right'
+					ml={1}
+					openDelay={500}
+					display={{ base: "block", md: "none" }}
+				>
+					<Link
+                display={"flex"}
+                to={"/auth"}
+                as={RouterLink}
+                alignItems={"center"}
+                gap={4}
+                _hover={"whiteAlpha.400"}
+                borderRadius={6}
+                p={2}
+                w={{base: 10, md:"full"}}
+				mt={"auto"}
+				justifyContent={{base: "center", md:"flex-start"}}
+              >
+				<BiLogOut size={25} />
+				<Box display={{base:"none",md:"block"}}>Logout</Box>
+
+			</Link>
+		</Tooltip>
+
       </Flex>
     </Box>
   );
---
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -0,0 +1,34 @@
+import { Box, Flex, Text, VStack } from "@chakra-ui/react"
+import SuggestedHeader from "./SuggestedHeader"
+import SuggestedUser from "./SuggestedUser";
+import { Link } from "react-router-dom";
+
+
+const SuggestedUsers = () => {
+  return (
+  <VStack py={8} px={6} gap={4}>
+    <SuggestedHeader />
+
+    <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+		<Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+			Suggested for you
+		</Text>
+		<Text fontSize={12} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"}>
+			See All
+		</Text>
+	</Flex>
+	<SuggestedUser name="Salman R" followers={-20} avatar=''/>
+    <SuggestedUser name="Rahul Kumar" followers={5} avatar=''/>
+    <SuggestedUser name="Udath Hegde" followers={999} avatar=''/>
+
+    <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
+				© 2024 Built By{" "}
+				<Link href='https://github.com/nife77' target='_blank' color='blue.500' fontSize={14}>
+					Nife
+				</Link>
+			</Box>
+</VStack>
+  );
+}
+
+export default SuggestedUsers
